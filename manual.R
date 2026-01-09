d4a8c1f616d0051fa222b8ada69b99897aace46e
milin-zhang <milin.zhang@tu-ilmenau.de>
2026-01-09 18:13:57 +0100

    regression

--- manual.R
+++ manual.R
@@ -15,4 +15,15 @@ browseURL("docs/index.html")
 # 4. Open the Github Repo link
 usethis::browse_github()
 
+# extra: renew credentials
+## reset tokens (classic! not the fine-grained)
 gitcreds::gitcreds_set()
+
+## connect to the account
+usethis::git_sitrep()
+
+## change the user if necessary
+usethis::use_git_config(
+  user.name = "milin-zhang", 
+  user.email = "milin.zhang@tu-ilmenau.de"
+)
